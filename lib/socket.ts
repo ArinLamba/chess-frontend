44dd827147330a01f94a6703766103ad9cff46da
ArinLamba <arinlamba5@gmail.com>
2025-10-08 02:57:40 +0530

    checking after deployment

--- lib/socket.ts
+++ lib/socket.ts
@@ -2,7 +2,7 @@
 import { io, Socket } from "socket.io-client";
 
 export const socket: Socket = (() => {
-  return io(process.env.ORIGIN || "http://localhost:4000", {
+  return io(process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001", {
     transports: ["websocket", "polling"], // allow both
     withCredentials: true,
     autoConnect: true,
